0be9d9794b2f897c3e6fca38612937a84018e47f
Muskra <76000573+Muskra@users.noreply.github.com>
2024-01-22 22:57:58 +0100

    Add files via upload

--- main.go
+++ main.go
@@ -1,312 +1,314 @@
 package main
- 
-import(
-    "fmt"
-    "os"
-    "bufio"
+
+import (
+	"bufio"
+	"fmt"
+	"os"
 )
 
-const MEMORY_MAX int = 65536;
-var Memory [MEMORY_MAX]uint16 = [MEMORY_MAX]uint16{};
- 
+const MEMORY_MAX int = 65536
+
+var Memory [MEMORY_MAX]uint16 = [MEMORY_MAX]uint16{}
+
 // register enum
 const (
-    R_R0 = iota
-    R_R1
-    R_R2
-    R_R3
-    R_R4
-    R_R5
-    R_R6
-    R_R7
-    R_PC // program counter
-    R_COND
-    R_COUNT
+	R_R0 = iota
+	R_R1
+	R_R2
+	R_R3
+	R_R4
+	R_R5
+	R_R6
+	R_R7
+	R_PC // program counter
+	R_COND
+	R_COUNT
 )
- 
+
 // register storage
-var Registers [R_COUNT]uint16;
- 
+var Registers [R_COUNT]uint16
+
 // opcodes
 const (
-    OP_BR = iota    // branch
-    OP_ADD          // add
-    OP_LD           // load
-    OP_ST           // store
-    OP_JSR          // jump register
-    OP_AND          // bitwise and
-    OP_LDR          // load register
-    OP_STR          // store register
-    OP_RTI          // unused
-    OP_NOT          // bitwise not
-    OP_LDI          // load indirect
-    OP_STI          // store indirect
-    OP_JMP          // jump
-    OP_RES          // reserved (unused)
-    OP_LEA          // load effective address
-    OP_TRAP         // execute trap
-)
- 
-const (
-    FL_POS = (1 << 0) // P
-    FL_ZRO = (1 << 1) // Z
-    FL_NEG = (1 << 2) // N
+	OP_BR   = iota // branch
+	OP_ADD         // add
+	OP_LD          // load
+	OP_ST          // store
+	OP_JSR         // jump register
+	OP_AND         // bitwise and
+	OP_LDR         // load register
+	OP_STR         // store register
+	OP_RTI         // unused
+	OP_NOT         // bitwise not
+	OP_LDI         // load indirect
+	OP_STI         // store indirect
+	OP_JMP         // jump
+	OP_RES         // reserved (unused)
+	OP_LEA         // load effective address
+	OP_TRAP        // execute trap
 )
 
 const (
-    TRAP_GETC = 0x20    // get character from keyboardn bot echoed to the terminal
-    TRAP_OUT = 0x21     // output a character
-    TRAP_PUTS = 0x22    // output a word string
-    TRAP_IN = 0x23      // get character from keyboard, echoed to the terminal
-    TRAP_PUTSP = 0x24   // output a byte string
-    TRAP_HALT = 0x25    // halt the program
+	FL_POS = (1 << 0) // P
+	FL_ZRO = (1 << 1) // Z
+	FL_NEG = (1 << 2) // N
 )
- 
+
+const (
+	TRAP_GETC  = 0x20 // get character from keyboardn bot echoed to the terminal
+	TRAP_OUT   = 0x21 // output a character
+	TRAP_PUTS  = 0x22 // output a word string
+	TRAP_IN    = 0x23 // get character from keyboard, echoed to the terminal
+	TRAP_PUTSP = 0x24 // output a byte string
+	TRAP_HALT  = 0x25 // halt the program
+)
+
 func main() {
- 
-    if len(os.Args) < 2 {
-        fmt.Printf("lc3 [image-file] ...\n")
-        os.Exit(2)
-    }
- 
-    for j := 1 ; j < len(os.Args) ; j = j + j {
-        if true {//!read_image(os.Args[j]) {
-            fmt.Printf("failed to load image: %s\n", os.Args[j]);
-            os.Exit(1)
-        }
-    }
- 
-    Registers[R_COND] = FL_ZRO
- 
-    const (
-        PC_START = 0x3000
-    )
- 
-    Registers[R_PC] = PC_START
- 
-    var running bool = true;
-    for (running) {
-        var instr uint16 = mem_read(Registers[R_PC] + 1);
-        var op uint16 = instr >> 12;
- 
-        switch op
-        {
-            case OP_ADD:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var r1 uint16 = (instr >> 6) & 0x7;
-                var imm_flag uint16 = (instr >> 5) < 0x1;
- 
-                if (imm_flag) {
-                    var imm5 uint16 = sign_extend(instr & 0x1F, 5);
-                    Registers[r0] = Registers[r1] + imm5
-                } else {
-                    var r2 uint16 = instr & 0x7;
-                    Registers[r0] = Registers[r1] + Registers[r2]
-                }
-                update_flags(r0)
-                break;
- 
-            case OP_AND:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var r1 uint16 = (instr >> 6) & 0x7;
-                var imm_flag uint16 = (instr >> 5) < 0x1;
- 
-                if (imm_flag) {
-                    var imm5 uint16 = sign_extend(instr & 0x1F, 5);
-                    Registers[r0] = Registers[r1] & imm5
-                } else {
-                    var r2 uint16 = instr & 0x7;
-                    Registers[r0] = Registers[r1] & Registers[r2]
-                }
-                update_flags(r0)
-                break;
- 
-            case OP_NOT:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var r1 uint16 = (instr >> 6) & 0x7;
- 
-                Registers[r0] = ~Registers[r1]
-                update_flags(r0)
-                break;
- 
-            case OP_BR:
-                var n uint16 = instr & 0x11;
-                var z uint16 = instr & 0x10;
-                var p uint16 = instr & 0x9;
-                var pc_offset = sign_extend(instr & 0x8, 0);
- 
-                if (n & FL_NEG) | (z & FL_ZRO) | (p & FL_POS) {
-                    Registers[R_PC] = Registers[R_PC] + sign_extend(pc_offset)
-                }
-                break;
- 
-            case OP_JMP:
-                var r1 uint16 = (instr >> 6) & 0x7;
-                Registers[R_PC] = r1
-                break;
- 
-            case OP_JSR:
-                var long_flag = (instr >> 11) & 1;
-                Registers[R_R7] = Registers[R_PC]
 
-                if (long_flag) {
-                    var long_pc_offset uint16 = sign_extend(instr & 0x7FF, 11);
-                    Registers[R_PC] += long_pc_offset // JSR
-                } else {
-                    var r1 uint16 = (instr >> 6) & 0x7;
-                    Registers[R_PC] = Registers[r1] // JSRR
-                }
-                break;
- 
-            case OP_LD:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var pc_offset = sign_extend(instr & 0x1FF, 9);
- 
-                Registers[r0] = mem_read(Registers[R_PC] + pc_offset);
-                update_flags(r0)
-                break;
- 
-            case OP_LDI:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var pc_offset = sign_extend(instr & 0x1FF, 9);
- 
-                Registers[r0] = mem_read(mem_read(Registers[R_PC] + pc_offset));
-                update_flags(r0)
-                break;
- 
-            case OP_LDR:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var r1 = (instr >> 6) & 0x7;
-                var offset uint16 = sign_extend(instr & 0x3F, 6);
- 
-                Registers[r0] = mem_read(Registers[r1] + offset)
-                update_flags(r0)
-                break;
- 
-            case OP_LEA:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var pc_offset = sign_extend(instr & 0x1FF, 9);
- 
-                Registers[r0] = Registers[R_PC] + pc_offset
-                update_flags(r0)
-                break;
- 
-            case OP_ST:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var pc_offset = sign_extend(instr & 0x1FF, 9);
- 
-                mem_write(Registers[R_PC] + pc_offset, Registers[r0])
-                break;
+	if len(os.Args) < 2 {
+		fmt.Printf("lc3 [image-file] ...\n")
+		os.Exit(2)
+	}
 
-            case OP_STI:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var pc_offset = sign_extend(instr & 0x1FF, 9);
- 
-                mem_write(mem_read(Registers[R_PC] + pc_offset), Registers[r0])
-                break;
- 
-            case OP_STR:
-                var r0 uint16 = (instr >> 9) & 0x7;
-                var r1 uint16 = (instr >> 6) & 0x7;
-                var offset uint16 = sign_extend(instr & 0x3F, 6);
+	for j := 1; j < len(os.Args); j = j + j {
+		if true { //!read_image(os.Args[j]) {
+			fmt.Printf("failed to load image: %s\n", os.Args[j])
+			os.Exit(1)
+		}
+	}
 
-                mem_write(Registers[r1] + offset, Registers[r0]);
-                break;
+	Registers[R_COND] = FL_ZRO
 
-            case OP_TRAP:
-                switch (instr & 0xFF) {
-                    case TRAP_GETC:
-                        Registers[R_R0] = os.Stdin.Read()
-                        update_flags(R_R0);
-                        break;
+	const (
+		PC_START = 0x3000
+	)
 
-                    case TRAP_OUT:
-                var ch byte = Registers[R_R0][7:];
-                        writer := bufio.NewWriter(os.Stdout)
-                        fmt.Fprintf(writer, ch)
-                        writer.Flush();
-                        break;
+	Registers[R_PC] = PC_START
 
-                    case TRAP_PUTS:
-                        var c *uint16 = Memory + Registers[R_R0];
-                        writer := bufio.NewWriter(os.Stdout)
-                        for (*c) {
-                            fmt.Fprintf(writer, c)
-                            *c = 1 + *c
-                            c = c + 1
-                        }
-                        writer.Flush();
-                        break;
+	var running bool = true
+	for running {
+		var instr uint16 = mem_read(Registers[R_PC] + 1)
+		var op uint16 = instr >> 12
 
-                    case TRAP_IN:
-                        writer := bufio.NewWriter(os.Stdout)
-                        fmt.Fprintf(writer, "$_ ")
-                        ch := os.Stdin.Read()
-                        fmt.Fprintf("%c", ch)
-                        writer.Flush();
-                        Registers[R_R0] = uint16(ch)
-                        update_flags(Registers[R_R0])
-                        break;
+		switch op {
+		case OP_ADD:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var r1 uint16 = (instr >> 6) & 0x7
+			var imm_flag uint16 = (instr >> 5) & 0x1
 
-                    case TRAP_PUTSP:
-                        var c *uint16 = Memory + Registers[R_R0];
-                        writer := bufio.NewWriter(os.Stdout)
-                        for (*c) { 
-                            var ch1 byte = byte(*c & 0xFF);
-                            fmt.Fprintf(writer, ch1)
-                            var ch2 byte = *c >> 8;
-                            if (ch2) { fmt.Fprintf(writer, ch2) };
-                            c = c + 1
-                        }
-                        writer.Flush();
-                        break;
+			if imm_flag {
+				var imm5 uint16 = sign_extend(instr&0x1F, 5)
+				Registers[r0] = Registers[r1] + imm5
+			} else {
+				var r2 uint16 = instr & 0x7
+				Registers[r0] = Registers[r1] + Registers[r2]
+			}
+			update_flags(r0)
+			break
 
-                    case TRAP_HALT:
-                        fmt.Println("Program halt.")
-                        os.Exit(0)
-                }
-                break;
-            case OP_RES:
-                abort("BAD OPCODE: 'RES'")
-            case OP_RTI:
-                abort("BAD OPCODE: 'RTI'")
-            default:
-                
-                break;
-        }
- 
-    }
- 
+		case OP_AND:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var r1 uint16 = (instr >> 6) & 0x7
+			var imm_flag uint16 = (instr >> 5) & 0x1
+
+			if imm_flag {
+				var imm5 uint16 = sign_extend(instr&0x1F, 5)
+				Registers[r0] = Registers[r1] & imm5
+			} else {
+				var r2 uint16 = instr & 0x7
+				Registers[r0] = Registers[r1] & Registers[r2]
+			}
+			update_flags(r0)
+			break
+
+		case OP_NOT:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var r1 uint16 = (instr >> 6) & 0x7
+
+			Registers[r0] = ~Registers[r1]
+			update_flags(r0)
+			break
+
+		case OP_BR:
+			var n uint16 = instr & 0x11
+			var z uint16 = instr & 0x10
+			var p uint16 = instr & 0x9
+			var pc_offset = sign_extend(instr&0x8, 0)
+
+			if (n & FL_NEG) | (z & FL_ZRO) | (p & FL_POS) {
+				Registers[R_PC] = Registers[R_PC] + sign_extend(pc_offset)
+			}
+			break
+
+		case OP_JMP:
+			var r1 uint16 = (instr >> 6) & 0x7
+			Registers[R_PC] = r1
+			break
+
+		case OP_JSR:
+			var long_flag = (instr >> 11) & 1
+			Registers[R_R7] = Registers[R_PC]
+
+			if long_flag {
+				var long_pc_offset uint16 = sign_extend(instr&0x7FF, 11)
+				Registers[R_PC] += long_pc_offset // JSR
+			} else {
+				var r1 uint16 = (instr >> 6) & 0x7
+				Registers[R_PC] = Registers[r1] // JSRR
+			}
+			break
+
+		case OP_LD:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var pc_offset = sign_extend(instr&0x1FF, 9)
+
+			Registers[r0] = mem_read(Registers[R_PC] + pc_offset)
+			update_flags(r0)
+			break
+
+		case OP_LDI:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var pc_offset = sign_extend(instr&0x1FF, 9)
+
+			Registers[r0] = mem_read(mem_read(Registers[R_PC] + pc_offset))
+			update_flags(r0)
+			break
+
+		case OP_LDR:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var r1 = (instr >> 6) & 0x7
+			var offset uint16 = sign_extend(instr&0x3F, 6)
+
+			Registers[r0] = mem_read(Registers[r1] + offset)
+			update_flags(r0)
+			break
+
+		case OP_LEA:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var pc_offset = sign_extend(instr&0x1FF, 9)
+
+			Registers[r0] = Registers[R_PC] + pc_offset
+			update_flags(r0)
+			break
+
+		case OP_ST:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var pc_offset = sign_extend(instr&0x1FF, 9)
+
+			mem_write(Registers[R_PC]+pc_offset, Registers[r0])
+			break
+
+		case OP_STI:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var pc_offset = sign_extend(instr&0x1FF, 9)
+
+			mem_write(mem_read(Registers[R_PC]+pc_offset), Registers[r0])
+			break
+
+		case OP_STR:
+			var r0 uint16 = (instr >> 9) & 0x7
+			var r1 uint16 = (instr >> 6) & 0x7
+			var offset uint16 = sign_extend(instr&0x3F, 6)
+
+			mem_write(Registers[r1]+offset, Registers[r0])
+			break
+
+		case OP_TRAP:
+			switch instr & 0xFF {
+			case TRAP_GETC:
+				Registers[R_R0] = os.Stdin.Read()
+				update_flags(R_R0)
+				break
+
+			case TRAP_OUT:
+				var ch byte = Registers[R_R0][7:]
+				writer := bufio.NewWriter(os.Stdout)
+				fmt.Fprintf(writer, ch)
+				writer.Flush()
+				break
+
+			case TRAP_PUTS:
+				var c *uint16 = Memory + Registers[R_R0]
+				writer := bufio.NewWriter(os.Stdout)
+				for *c {
+					fmt.Fprintf(writer, c)
+					*c = 1 + *c
+					c = c + 1
+				}
+				writer.Flush()
+				break
+
+			case TRAP_IN:
+				writer := bufio.NewWriter(os.Stdout)
+				fmt.Fprintf(writer, "$_ ")
+				ch := os.Stdin.Read()
+				fmt.Fprintf("%c", ch)
+				writer.Flush()
+				Registers[R_R0] = uint16(ch)
+				update_flags(Registers[R_R0])
+				break
+
+			case TRAP_PUTSP:
+				var c *uint16 = Memory + Registers[R_R0]
+				writer := bufio.NewWriter(os.Stdout)
+				for *c {
+					var ch1 byte = byte(*c & 0xFF)
+					fmt.Fprintf(writer, ch1)
+					var ch2 byte = *c >> 8
+					if ch2 {
+						fmt.Fprintf(writer, ch2)
+					}
+					c = c + 1
+				}
+				writer.Flush()
+				break
+
+			case TRAP_HALT:
+				fmt.Println("Program halt.")
+				os.Exit(0)
+			}
+			break
+		case OP_RES:
+			abort("BAD OPCODE: 'RES'")
+		case OP_RTI:
+			abort("BAD OPCODE: 'RTI'")
+		default:
+
+			break
+		}
+
+	}
+
 }
- 
+
 func sign_extend(x uint16, bit_count int) uint16 {
-    if ( (x >> ( bit_count - 1 ) ) & 1 ) {
-        x = x | (0xFFF << bit_count)
-    }
-    return x
+	if (x >> (bit_count - 1)) & 1 {
+		x = x | (0xFFF << bit_count)
+	}
+	return x
 }
- 
+
 func update_flags(r uint16) {
-    if Registers[r] == 0 {
-        Registers[R_COND] = FL_ZRO
-    } else if (Registers[r] >> 15) {
-        Registers[R_COND] = FL_NEG
-    } else {
-        Registers[R_COND] = FL_POS
-    }
+	if Registers[r] == 0 {
+		Registers[R_COND] = FL_ZRO
+	} else if Registers[r] >> 15 {
+		Registers[R_COND] = FL_NEG
+	} else {
+		Registers[R_COND] = FL_POS
+	}
 }
 
 func mem_read(a uint16) uint16 {
-    var x uint16 = 0;
-    return x
-};
-func mem_write(a int, b uint16) int { return 0 }; 
+	var x uint16 = 0
+	return x
+}
+func mem_write(a int, b uint16) int { return 0 }
 func read_image(a string) uint16 {
-    var x uint16 = 0;
-    return x
-};
- 
+	var x uint16 = 0
+	return x
+}
+
 func abort(str string) {
-    fmt.Printf("<!> Error:\t%s", str)
-    os.Exit(1)
+	fmt.Printf("<!> Error:\t%s", str)
+	os.Exit(1)
 }
